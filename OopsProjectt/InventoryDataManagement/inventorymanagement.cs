d0f1917682f5ac75fce1b1ac8388dc0283878b4c
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-25 15:31:37 +0530

    [shashidhar] Add: programme for JASON INVENTORY DATA updated

---
+++ OopsProjectt/InventoryDataManagement/inventorymanagement.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OopsProjectt.InventoryDataManagement
+{
+    public class inventorymanagement
+    {
+        public void DisplayInventory(string filepath)
+        {
+            using (StreamReader reader = new StreamReader(filepath))
+            { 
+            var json = reader.ReadToEnd();
+            var inventory = JsonConvert.DeserializeObject<List<InventoryData>>(json);
+                Console.WriteLine("Name"+"\t"+"weight"+"\t"+"priceperkg"+"\t"+"totalprice");
+                foreach (var data in inventory)
+                { 
+                Console.WriteLine(data.Name+"\t"+data.Weight+"\t"+data.PricePerKg+"\t\t"+(data.Weight*data.PricePerKg));
+                }
+            }
+        }
+    }
+}
---
+++ OopsProjectt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OopsProjectt.InventoryDataManagement
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to inventory");
+            const string Inventory_Path = @"C:\oops programme\JsonProject\OopsProjectt\InventoryDataManagement\Inventory.json";
+            inventorymanagement data = new inventorymanagement();
+            Console.WriteLine("Display 1\n");
+            bool check = true;
+            while (check)
+            { 
+            Console.WriteLine("Enter your option to execute: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        data.DisplayInventory(Inventory_Path);
+                        break;
+                        case 2:
+                        check = false;
+                        break;
+                }
+            }
+        }
+    
+    }
+
+}
